fe2d12ec0c59f1ffeb3a3f8221b9d946b51b8a82
Steven A. Goodman <steven.a.goodman@outlook.com>
2024-11-21 06:51:25 -0500

    progress on parsing pngs

--- path_creator.cs
+++ path_creator.cs
@@ -1,4 +1,10 @@
-// png parser
-// identify points of contrast
-  // maybe include something to find the avg circle
-// export to json
+// using System; 
+// using System.IO; 
+
+// // png parser
+// string input_file = "example.png"
+// byte[] png_bytes = File.ReadAllBytes(input_file)
+
+// // identify points of contrast
+//   // maybe include something to find the avg circle
+// // export to json
